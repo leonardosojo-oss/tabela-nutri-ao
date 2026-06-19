163b203bb15218b122c01c2d7144285b244b2d9c
leonardosojo-oss <leonardo.sojo@escola.pr.gov.br>
2026-06-19 12:29:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 var titulo2 = document.querySelector("h2")
-titulo2.textContent = "Clientes"
+titulo2.textContent = "Clientes";
+
+//peso
+let paciente = document.querySelector("#primeiro-paciente");
+let tdPeso = paciente.querySelector(".info-peso");
+
+//altura
+let tdAltura = paciente.querySelector(".info-altura");
+let valorAltura = tdAltura.textContent;
+
+//clculo imc
+let imc = valorPeso / (valorAltura * valorAltura);
+
+//valor imc
+let tdImc = paciente.querySelector('.info-imc');
+tdImc.textContent = imc
+
+//verificaçao de peso e altura
+if(valordoPeso <0 || valorPeso > 350){
+    alert("Peso Invalida!");
+}
